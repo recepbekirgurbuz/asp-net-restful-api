3e8c80beabfe14a43e3f8068edc7b9743c0b1c10
Recep Bekir Gurbuz <recepbekirgrz9@gmail.com>
2024-11-22 21:41:44 +0300

    Account and payment controllers created

---
+++ Controllers/PaymentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace asp_net_restful_api.Controllers
+{
+    public class PaymentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
